5e4c44ce366d5bb6b29c76f8168f325514bd55b6
Tobis <69050009+Tobiswaggy@users.noreply.github.com>
2024-01-30 14:02:49 +0100

    Update batch.bat

--- batch.bat
+++ batch.bat
@@ -1,19 +1,17 @@
 @echo off
 
-        set File="uzivatele.txt"
-
     for /f "tokens=1-3 delims=,", %%a in (uzivatele.txt) do (
 
-        set "Usr=%%a"
-        set "Pwd=%%b"
-        set "Group=%%c"
+        net user "%%a" %%b /add
 
-        net user "%Usr%" %Pwd% /add
+    echo Byl tvoren uzivatel "%%a"
 
-    echo Byl tvoren uzivatel %Usr%.
+        net localgroup %%c /add
 
-        net localgroup "%Group%" "%Usr%" /add
+    echo Byla vytvorena skupina "%%c"
 
-    echo Uzivatel %Usr% byl pridan do skupiny %Group%
+        net localgroup "%%c" "%%a" /add
+
+    echo Uzivatel %%a byl pridan do skupiny %%c
 
     )
